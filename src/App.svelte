1d7775c8fa06e42008b7245d0b420ff5315421e8
Melody Vang <melodyvang@college.harvard.edu>
2023-01-29 17:22:14 -0500

    starter files

--- src/App.svelte
+++ src/App.svelte
@@ -2,4 +2,17 @@
   // component logic
 </script>
 
-<!-- component markup -->
+<main>
+  <div class="container">
+    <h1 class="app-title">todos</h1>
+    <ul class="todo-list"></ul>
+    <div class="empty-state">
+      <svg class="checklist-icon"><use href="#checklist-icon"></use></svg>
+      <h2 class="empty-state__title">Add your first todo</h2>
+      <p class="empty-state__description">What do you want to get done today?</p>
+    </div>
+    <form>
+      <input class="js-todo-input" type="text" aria-label="Enter a new todo item" placeholder="E.g. Build a web app" />
+    </form>
+  </div>
+</main>
